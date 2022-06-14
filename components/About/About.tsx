f2cdeb2e1a3fbd8dbd249130d2c3a63eba814ad8
mariandotg <marianguillaume.m@gmail.com>
2022-06-14 09:24:38 -0300

    Added About base component

---
+++ components/About/About.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <section>
+      <img className='w-64 h-64' src='./me.png' alt='Photo of me' />
+      <h1>Title</h1>
+      <p>paragraph 1</p>
+      <p>paragraph 2</p>
+    </section>
+  );
+};
+
+export default About;
