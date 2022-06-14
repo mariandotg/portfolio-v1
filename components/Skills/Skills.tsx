2191898d5eed5fb8a94bbca1f0472d6175c1e959
mariandotg <marianguillaume.m@gmail.com>
2022-06-14 17:55:28 -0300

    Refactored Skills component

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,37 +1,43 @@
 const Skills = () => {
   return (
-    <>
-      <h2>Skills</h2>
-      <section>
-        <div>
-          <h3>Section subtitle 1</h3>
-          <ul>
+    <section className='flex flex-col gap-4'>
+      <h2 className='font-bold text-section-title text-light-accent'>Skills</h2>
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Section subtitle 1
+          </h3>
+          <ul className='ml-4 list-disc'>
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
             <li>Item 4</li>
           </ul>
         </div>
-        <div>
-          <h3>Section subtitle 2</h3>
-          <ul>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Section subtitle 2
+          </h3>
+          <ul className='ml-4 list-disc'>
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
             <li>Item 4</li>
           </ul>
         </div>
-        <div>
-          <h3>Section subtitle 3</h3>
-          <ul>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Section subtitle 3
+          </h3>
+          <ul className='ml-4 list-disc'>
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
             <li>Item 4</li>
           </ul>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
