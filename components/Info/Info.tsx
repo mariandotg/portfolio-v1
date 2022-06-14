ce24e2266884db42ef2a8a563b3500b60841f919
mariandotg <marianguillaume.m@gmail.com>
2022-06-14 09:28:31 -0300

    Added Info base component

---
+++ components/Info/Info.tsx
@@ -0,0 +1,17 @@
+const Info = () => {
+  return (
+    <>
+      <h2>Info</h2>
+      <section>
+        <h3>Subtitle 1</h3>
+        <p>Subtitle 1</p>
+        <h3>Subtitle 2</h3>
+        <p>Subtitle 2</p>
+        <h3>Subtitle 3</h3>
+        <p>Subtitle 3</p>
+      </section>
+    </>
+  );
+};
+
+export default Info;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import About from '../components/About/About';
+import Info from '../components/Info/Info';
 
 const Home: NextPage = () => {
   return (
     <div>
       <About />
+      <Info />
     </div>
   );
 };
