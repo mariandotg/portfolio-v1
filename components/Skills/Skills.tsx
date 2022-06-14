0dcc0d25b8e3477e9df6c76c78d150e9d60a3c62
mariandotg <marianguillaume.m@gmail.com>
2022-06-14 09:31:58 -0300

    Added Skills base component

---
+++ components/Skills/Skills.tsx
@@ -0,0 +1,38 @@
+const Skills = () => {
+  return (
+    <>
+      <h2>Skills</h2>
+      <section>
+        <div>
+          <h3>Section subtitle 1</h3>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Section subtitle 2</h3>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Section subtitle 3</h3>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Skills;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import About from '../components/About/About';
 import Info from '../components/Info/Info';
+import Skills from '../components/Skills/Skills';
 
 const Home: NextPage = () => {
   return (
     <div>
       <About />
       <Info />
+      <Skills />
     </div>
   );
 };
