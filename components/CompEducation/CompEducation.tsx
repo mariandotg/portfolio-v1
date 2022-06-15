724a4bed67d1d2c863fcabfd123793bf9df95f37
mariandotg <marianguillaume.m@gmail.com>
2022-06-14 21:07:20 -0300

    Refactored CompEducation component

--- components/CompEducation/CompEducation.tsx
+++ components/CompEducation/CompEducation.tsx
@@ -1,15 +1,40 @@
 const CompEducation = () => {
   return (
-    <>
-      <h2>Education</h2>
-      <section>
-        <div>
-          <h3>Course title</h3>
-          <p>Period</p>
+    <section className='flex flex-col gap-4'>
+      <h2 className='font-bold text-section-title text-light-accent'>
+        Education
+      </h2>
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Course title
+          </h3>
+          <p className='text-disabled'>Period</p>
           <p>Description</p>
         </div>
-      </section>
-    </>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Course title
+          </h3>
+          <p className='text-disabled'>Period</p>
+          <p>Description</p>
+        </div>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Course title
+          </h3>
+          <p className='text-disabled'>Period</p>
+          <p>Description</p>
+        </div>
+        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
+          <h3 className='font-bold text-section-subtitle text-light-accent'>
+            Course title
+          </h3>
+          <p className='text-disabled'>Period</p>
+          <p>Description</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
