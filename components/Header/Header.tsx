4826bb30af4dc7e420ee185aa614d24162e42577
mariandotg <marianguillaume.m@gmail.com>
2022-06-17 11:57:09 -0300

    Modified button in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,10 @@
 import Button from '../Buttons/Button';
-import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
     <>
       <header className='p-4 bg-light-card-bg fixed w-full'>
-        <Button variant='primary' icon>
-          <MenuIcon />
-        </Button>
+        <Button variant='primary'>Download CV</Button>
       </header>
     </>
   );
