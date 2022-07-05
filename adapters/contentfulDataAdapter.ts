933bf6aa9f61ad83b3cb4620cd5ca45626d23f91
mariandotg <marianguillaume.m@gmail.com>
2022-07-05 18:56:48 -0300

    Implemented new contentfulDataAdapter function

--- adapters/contentfulDataAdapter.ts
+++ adapters/contentfulDataAdapter.ts
@@ -1,19 +1,35 @@
-import { CardEntry, DataFormatted, SectionEntry } from '../models/data';
+import {
+  CardEntry,
+  Card,
+  DataFormatted,
+  SectionEntry,
+  Item,
+  ItemEntry,
+} from '../models/data';
 
 const contentfulDataAdapter = (data: Array<SectionEntry>) => {
   const formattedData = data.reduce<DataFormatted>(
     (acc: DataFormatted, section: SectionEntry) => {
       const cards = section.fields.cards
-        ? section.fields.cards.reduce(
-            (acc: object, card: CardEntry, index: number) => {
+        ? section.fields.cards.reduce<Card[]>(
+            (acc: Card[], card: CardEntry) => {
               const rest = { ...card.fields, id: card.sys.id };
-              return { ...acc, [index]: rest };
+              return [...acc, rest];
             },
-            {}
+            []
           )
         : null;
-      const rest = { ...section.fields, cards };
-      return { ...acc, [section.fields.id]: rest };
+      const items = section.fields.items
+        ? section.fields.items.reduce<Item[]>(
+            (acc: Item[], item: ItemEntry) => {
+              const rest = { ...item.fields, id: item.sys.id };
+              return [...acc, rest];
+            },
+            []
+          )
+        : null;
+      const rest = { ...section.fields, cards, items, id: section.sys.id };
+      return { ...acc, [section.fields.key]: rest };
     },
     {}
   );
