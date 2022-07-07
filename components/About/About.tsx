2f0e6f9cd671b9d0b780088b49ecd005d3d01132
mariandotg <marianguillaume.m@gmail.com>
2022-07-06 21:47:23 -0300

    Added dynamic data

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,10 +1,25 @@
+import { useSelector } from 'react-redux';
+import { selectAbout } from '../../app/store/slices/data';
+import splitString from '../../utils/splitString';
+
 const About = () => {
+  const data = useSelector(selectAbout);
+  const { title, description } = data!;
+
+  const titleFormatted = splitString(title!);
+  const descriptionFormatted = splitString(description!);
+
   return (
     <section className='flex flex-col gap-4'>
       <img className='rounded-base' src='./me.png' alt='Photo of me' />
-      <div className='p-4 bg-light-card-bg rounded-base'>
-        <h1 className='font-bold text-section-subtitle'>Title</h1>
-        <p>Paragraph </p>
+      <div className='p-4 bg-light-card-bg rounded-base flex flex-col gap-2'>
+        <h1 className='font-bold text-section-subtitle'>
+          <span>{titleFormatted[0]}</span>
+          <span className='text-light-accent'>{titleFormatted[1]}</span>
+        </h1>
+        {descriptionFormatted.map((p, index) => (
+          <p key={index}>{p}</p>
+        ))}
       </div>
     </section>
   );
