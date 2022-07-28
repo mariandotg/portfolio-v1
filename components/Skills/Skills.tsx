8cb811d4cbe335b967d36aca5db8bcb936514fe5
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 13:35:26 -0300

    Added dynamic data to the Skills component

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,41 +1,31 @@
+import { useSelector } from 'react-redux';
+import { selectSkills } from '../../app/store/slices/data';
+import { Card } from '../../models/data';
+
 const Skills = () => {
+  const data = useSelector(selectSkills);
+  const { title, cards } = data!;
   return (
     <section className='flex flex-col gap-4'>
-      <h2 className='font-bold text-section-title text-light-accent'>Skills</h2>
+      <h2 className='font-bold text-section-title text-light-accent'>
+        {title}
+      </h2>
       <div className='grid grid-cols-2 gap-4'>
-        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
-          <h3 className='font-bold text-section-subtitle text-light-accent'>
-            Section subtitle 1
-          </h3>
-          <ul className='ml-4 list-disc'>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-          </ul>
-        </div>
-        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
-          <h3 className='font-bold text-section-subtitle text-light-accent'>
-            Section subtitle 2
-          </h3>
-          <ul className='ml-4 list-disc'>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-          </ul>
-        </div>
-        <div className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'>
-          <h3 className='font-bold text-section-subtitle text-light-accent'>
-            Section subtitle 3
-          </h3>
-          <ul className='ml-4 list-disc'>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-          </ul>
-        </div>
+        {cards!.map((s: Card) => (
+          <div
+            key={s.id}
+            className='flex flex-col gap-2 p-4 aspect-square bg-light-card-bg rounded-base'
+          >
+            <h3 className='font-bold text-section-subtitle text-light-accent'>
+              {s.subtitle}
+            </h3>
+            <ul className='ml-4 list-disc'>
+              {s.techs!.map((t: string, i: number) => (
+                <li key={i}>{t}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
