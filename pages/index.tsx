4a386bb5efa5410c43f845e68875a78df10d6ade
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 15:51:10 -0300

    Cleaned home page and deleted CompEducation comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,30 @@
 import type { GetServerSideProps, NextPage } from 'next';
-import { useSelector } from 'react-redux';
 
+import Header from '../components/Header/Header';
 import About from '../components/About/About';
 import Info from '../components/Info/Info';
 import Skills from '../components/Skills/Skills';
-import JobExperience from '../components/JobExperience/JobExperience';
 import Education from '../components/Education/Education';
-import CompEducation from '../components/CompEducation/CompEducation';
+import JobExperience from '../components/JobExperience/JobExperience';
 import FeaturedProjects from '../components/FeaturedProjects/FeaturedProjects';
-import Header from '../components/Header/Header';
-import { fetchData, selectData } from '../app/store/slices/data';
-import { wrapper } from '../app';
 import Footer from '../components/Footer/Footer';
 
+import { fetchData } from '../app/store/slices/data';
+import { wrapper } from '../app';
+
 const Home: NextPage = () => {
-  const data = useSelector(selectData);
-  console.log('testing', data);
   return (
     <>
       <Header />
       <div className='h-[92px]'></div>
-      <div className='flex flex-col gap-4 p-8'>
+      <div className='flex flex-col gap-16 p-8'>
         <About />
-        <Info />
-        <Skills />
-        <JobExperience />
-        <Education />
-        <CompEducation />
+        <div className='flex flex-col gap-8'>
+          <Info />
+          <Skills />
+          <Education />
+          <JobExperience />
+        </div>
         <FeaturedProjects />
       </div>
       <Footer />
