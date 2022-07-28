3c07f7f8f4891ce70040f832ede50e1e8b0e7613
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 14:50:48 -0300

    Refactored Info component

--- components/Info/Info.tsx
+++ components/Info/Info.tsx
@@ -8,15 +8,11 @@ const Info = () => {
 
   return (
     <section className='flex flex-col gap-4'>
-      <h2 className='font-bold text-section-title text-light-primary'>
-        {title}
-      </h2>
-      <div className='flex flex-col gap-2 p-4 bg-light-card-bg rounded-base'>
+      <h2 className='text-xl font-bold text-light-primary'>{title}</h2>
+      <div className='flex flex-col gap-4 p-4 bg-light-card-bg rounded-base'>
         {items!.map((i: Item) => (
-          <div key={i.id}>
-            <h3 className='font-bold text-section-subtitle text-light-primary'>
-              {i.label}
-            </h3>
+          <div key={i.id} className='flex flex-col gap-1'>
+            <h3 className='text-lg font-bold text-light-primary'>{i.label}</h3>
             <p>{i.item}</p>
           </div>
         ))}
