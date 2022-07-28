00c164155e64f5926815f217e7f1b7cf6e00f877
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 15:35:28 -0300

    Refactored Education component

--- components/Education/Education.tsx
+++ components/Education/Education.tsx
@@ -1,20 +1,28 @@
+import { useSelector } from 'react-redux';
+import { selectEducation } from '../../app/store/slices/data';
+import { Card } from '../../models/data';
+
 const Education = () => {
+  const data = useSelector(selectEducation);
+  const { title, cards } = data!;
+
   return (
     <section className='flex flex-col gap-4'>
-      <h2 className='font-bold text-section-title text-light-primary'>
-        Education
-      </h2>
-      <div className='flex flex-col gap-2'>
-        <div className='flex flex-col gap-2 p-4 bg-light-card-bg rounded-base'>
-          <div>
-            <h3 className='font-bold text-section-subtitle text-light-primary'>
-              College
-            </h3>
-            <h3 className='font-bold text-section-subtitle'>Title</h3>
+      <h2 className='text-xl font-bold text-light-primary'>{title}</h2>
+      <div className='flex flex-col gap-4'>
+        {cards!.map((e: Card) => (
+          <div
+            key={e.id}
+            className='flex flex-col gap-2 p-4 bg-light-card-bg rounded-base'
+          >
+            <div>
+              <h3 className='font-bold text-light-secondary'>College</h3>
+              <h3 className='text-xl font-bold text-light-primary'>Title</h3>
+            </div>
+            <p className='text-light-secondary'>{e.period}</p>
+            <p>{e.description}</p>
           </div>
-          <p className='text-disabled'>Period</p>
-          <p>Description</p>
-        </div>
+        ))}
       </div>
     </section>
   );
