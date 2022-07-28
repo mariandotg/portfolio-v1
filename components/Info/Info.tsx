85a5823f61db1d8851f8477dbdac796c6c044fa7
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 13:33:26 -0300

    Added dynamic data to the Info component

--- components/Info/Info.tsx
+++ components/Info/Info.tsx
@@ -1,20 +1,25 @@
+import { useSelector } from 'react-redux';
+import { selectInfo } from '../../app/store/slices/data';
+import { Item } from '../../models/data';
+
 const Info = () => {
+  const data = useSelector(selectInfo);
+  const { title, items } = data!;
+
   return (
     <section className='flex flex-col gap-4'>
-      <h2 className='font-bold text-section-title text-light-accent'>Info</h2>
+      <h2 className='font-bold text-section-title text-light-accent'>
+        {title}
+      </h2>
       <div className='flex flex-col gap-2 p-4 bg-light-card-bg rounded-base'>
-        <h3 className='font-bold text-section-subtitle text-light-accent'>
-          Subtitle 1
-        </h3>
-        <p>Subtitle 1</p>
-        <h3 className='font-bold text-section-subtitle text-light-accent'>
-          Subtitle 2
-        </h3>
-        <p>Subtitle 2</p>
-        <h3 className='font-bold text-section-subtitle text-light-accent'>
-          Subtitle 3
-        </h3>
-        <p>Subtitle 3</p>
+        {items!.map((i: Item) => (
+          <div key={i.id}>
+            <h3 className='font-bold text-section-subtitle text-light-accent'>
+              {i.label}
+            </h3>
+            <p>{i.item}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
