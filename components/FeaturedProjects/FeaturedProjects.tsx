1595f88848ef73a00b11ae6fd05ef879f36fb6f3
mariandotg <marianguillaume.m@gmail.com>
2022-07-28 15:58:08 -0300

    Refactored FeaturedProjects component

--- components/FeaturedProjects/FeaturedProjects.tsx
+++ components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,52 +1,38 @@
+import { useSelector } from 'react-redux';
+import { selectProjects } from '../../app/store/slices/data';
+import { Card } from '../../models/data';
 import Button from '../Buttons/Button';
 
 const FeaturedProjects = () => {
+  const data = useSelector(selectProjects);
+  const { title, cards } = data!;
+
   return (
     <section className='flex flex-col gap-4'>
-      <h2 className='font-bold text-section-title text-light-primary'>
-        Featured Projects
-      </h2>
+      <h2 className='text-xl font-bold text-light-primary'>{title}</h2>
       <div className='flex flex-col gap-4'>
-        <div>
-          <img
-            className='object-cover object-left-top w-full h-52 rounded-tl-base rounded-tr-base'
-            src='./me.png'
-            alt='project'
-          />
-          <div className='flex flex-col gap-4 p-4 bg-light-card-bg rounded-bl-base rounded-br-base'>
-            <div className='flex flex-col gap-2'>
-              <h3 className='font-bold text-section-subtitle text-light-primary'>
-                Project title
-              </h3>
-              <p className='text-disabled'>Period</p>
-              <p>Description</p>
-            </div>
-            <div className='flex self-end gap-4'>
-              <Button variant='secondary'>Button 2</Button>
-              <Button variant='primary'>Button 1</Button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <img
-            className='object-cover object-left-top w-full h-52 rounded-tl-base rounded-tr-base'
-            src='./me.png'
-            alt='project'
-          />
-          <div className='flex flex-col gap-4 p-4 bg-light-card-bg rounded-bl-base rounded-br-base'>
-            <div className='flex flex-col gap-2'>
-              <h3 className='font-bold text-section-subtitle text-light-primary'>
-                Project title
-              </h3>
-              <p className='text-disabled'>Period</p>
-              <p>Description</p>
-            </div>
-            <div className='flex self-end gap-4'>
-              <Button variant='secondary'>Button 2</Button>
-              <Button variant='primary'>Button 1</Button>
+        {cards!.map((project: Card) => (
+          <div key={project.id}>
+            <img
+              className='object-cover object-left-top w-full h-52 rounded-tl-base rounded-tr-base'
+              src='./me.png'
+              alt='project'
+            />
+            <div className='flex flex-col gap-4 p-4 bg-light-card-bg rounded-bl-base rounded-br-base'>
+              <div className='flex flex-col gap-2'>
+                <h3 className='text-xl font-bold text-light-primary'>
+                  {project.subtitle}
+                </h3>
+                <p className='text-light-secondary'>{project.period}</p>
+                <p>{project.description}</p>
+              </div>
+              <div className='flex self-end gap-4'>
+                <Button variant='secondary'>Button 2</Button>
+                <Button variant='primary'>Button 1</Button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
