a21e61c673a16efba1d48feea84ee540794170d7
mariandotg <marianguillaume.m@gmail.com>
2022-08-10 08:49:40 -0300

    Made a basic contact form with Formik and Yup

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,40 +1,100 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 import { selectData } from '../../app/store/slices/data';
 import { IContactField } from '../../models/data';
 import Button from '../Buttons/Button';
 
+interface IInitialValues {
+  [index: string]: string;
+  subject: string;
+  from: string;
+  message: string;
+}
+
 const Contact = () => {
   const data = useSelector(selectData);
   const { title, cards, cta } = data.sections.contact;
+
+  const initialValues: IInitialValues = {
+    subject: '',
+    from: '',
+    message: '',
+  };
+
+  const required = '* Campo obligatorio';
+
+  const validationSchema = Yup.object().shape({
+    subject: Yup.string()
+      .min(3, 'La cantidad mínima de caracteres es 4')
+      .required(required),
+    from: Yup.string().email('Debe ser un email válido').required(required),
+    message: Yup.string().required(required),
+  });
+
+  const onSubmit = () => {
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    }).then((res) => {
+      resetForm();
+      console.log(res);
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+      }
+    });
+  };
+
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+  const { handleSubmit, handleChange, values, resetForm } = formik;
+
   return (
     <section className='flex flex-col items-end gap-4'>
-      <h2 className='text-xl font-bold self-start text-light-primary dark:text-dark-primary'>
+      <h2 className='self-start text-xl font-bold text-light-primary dark:text-dark-primary'>
         {title}
       </h2>
-      {cards.map((e: IContactField) => (
-        <div
-          key={e.id}
-          className='flex flex-col w-full gap-1 text-light-secondary dark:text-dark-secondary focus-within:text-light-primary dark:focus-within:text-dark-primary'
-        >
-          <label className='text-lg font-bold '>{e.label}</label>
-          {e.type === 'textarea' ? (
-            <div className='h-[192px] px-4 py-3 rounded-[8px] focus:outline-none border border-light-secondary dark:border-dark-secondary focus-within:border-light-primary dark:focus-within:border-dark-primary'>
-              <textarea
-                placeholder={e.placeholder}
-                className='resize-none w-full h-full bg-transparent focus:outline-none text-light-text dark:text-dark-text'
-              ></textarea>
-            </div>
-          ) : (
-            <input
-              type={e.type}
-              placeholder={e.placeholder}
-              className='w-full px-4 py-3 rounded-[8px] bg-transparent border focus:outline-none border-light-secondary dark:border-dark-secondary focus:border-light-primary dark:focus:border-dark-primary text-light-text dark:text-dark-text'
-            ></input>
-          )}
-        </div>
-      ))}
-      <Button variant={cta.variant}>{cta.cta}</Button>
+      <form
+        onSubmit={handleSubmit}
+        className='flex flex-col items-end w-full gap-4'
+      >
+        {cards.map((e: IContactField) => (
+          <div
+            key={e.id}
+            className='flex flex-col w-full gap-1 text-light-secondary dark:text-dark-secondary focus-within:text-light-primary dark:focus-within:text-dark-primary'
+          >
+            <label className='font-bold '>{e.label}</label>
+            {e.type === 'textarea' ? (
+              <div className='h-[192px] px-4 py-3 rounded-[8px] focus:outline-none border border-light-secondary dark:border-dark-secondary focus-within:border-light-primary dark:focus-within:border-dark-primary'>
+                <textarea
+                  name={e.name}
+                  placeholder={e.placeholder}
+                  value={values[e.name]}
+                  onChange={handleChange}
+                  className='w-full h-full bg-transparent resize-none focus:outline-none text-light-text dark:text-dark-text'
+                ></textarea>
+              </div>
+            ) : (
+              <>
+                <input
+                  name={e.name}
+                  type={e.type}
+                  placeholder={e.placeholder}
+                  value={values[e.name]}
+                  onChange={handleChange}
+                  className='w-full px-4 py-3 rounded-[8px] bg-transparent border focus:outline-none border-light-secondary dark:border-dark-secondary focus:border-light-primary dark:focus:border-dark-primary text-light-text dark:text-dark-text'
+                ></input>
+              </>
+            )}
+          </div>
+        ))}
+        <Button variant={cta.variant}>{cta.cta}</Button>
+      </form>
     </section>
   );
 };
