5d05b95dfa080c18058610400b337c961fc2fc33
mariandotg <marianguillaume.m@gmail.com>
2022-08-11 17:58:54 -0300

    Added colors to the themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -27,6 +30,7 @@ module.exports = {
           'card-bg': '#FFFFFF',
           primary: '#5B0ED6',
           'primary-disabled': '#9A73D9',
+          'primary-hover': '#490BAB',
           secondary: '#4C5562',
           text: '#000000',
         },
@@ -35,6 +39,7 @@ module.exports = {
           'card-bg': '#141428',
           primary: '#A494FF',
           'primary-disabled': '#807B99',
+          'primary-hover': '#8376CC',
           secondary: '#A1A0AA',
           text: '#FFFFFF',
         },
