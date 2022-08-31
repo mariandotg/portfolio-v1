d2e216280c4bcc419688dd479aba2387d7c265e4
mariandotg <marianguillaume.m@gmail.com>
2022-08-31 16:08:22 -0300

    Refactored Job Experience section

--- components/JobExperience/JobExperience.tsx
+++ components/JobExperience/JobExperience.tsx
@@ -1,6 +1,9 @@
 import { useSelector } from 'react-redux';
 import { selectDataSections } from '../../app/store/slices/data';
+
 import { IJobCard } from '../../models/data';
+
+import JobCard from './JobCard';
 
 const JobExperience = () => {
   const data = useSelector(selectDataSections);
@@ -11,27 +14,11 @@ const JobExperience = () => {
       <h2 className='text-xl font-bold text-light-primary dark:text-dark-primary'>
         {title}
       </h2>
-      <div className='flex flex-col gap-4 mobile:grid mobile:grid-cols-2'>
-        {cards.map((card: IJobCard) => (
-          <div
-            key={card.id}
-            className='flex flex-col gap-2 p-4 bg-light-card-bg dark:bg-dark-card-bg rounded-base'
-          >
-            <div>
-              <h3 className='font-bold text-light-secondary dark:text-dark-secondary'>
-                {card.company}
-              </h3>
-              <h3 className='text-xl font-bold text-light-primary dark:text-dark-primary'>
-                {card.position}
-              </h3>
-            </div>
-            <p className='text-light-secondary dark:text-dark-secondary'>
-              {card.period}
-            </p>
-            <p>{card.description}</p>
-          </div>
+      <ul className='flex flex-col gap-4 mobile:grid mobile:grid-cols-2'>
+        {cards.map((job: IJobCard) => (
+          <JobCard key={job.id} job={job} />
         ))}
-      </div>
+      </ul>
     </section>
   );
 };
