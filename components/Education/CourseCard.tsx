cded7682ee5fd3aaa16c603846ca74640437b263
mariandotg <marianguillaume.m@gmail.com>
2022-09-04 12:39:26 -0300

    Added certificate Button on CourseCards

--- components/Education/CourseCard.tsx
+++ components/Education/CourseCard.tsx
@@ -1,4 +1,5 @@
 import { ICourseCard } from '../../models/data';
+import Button from '../Buttons/Button';
 
 interface Props {
   course: ICourseCard;
@@ -6,19 +7,30 @@ interface Props {
 
 const CourseCard = ({ course }: Props) => {
   return (
-    <li className='flex flex-col gap-2 p-4 bg-light-card-bg dark:bg-dark-card-bg rounded-base'>
-      <div>
-        <h3 className='font-bold text-light-secondary dark:text-dark-secondary'>
-          {course.institution}
-        </h3>
-        <h3 className='text-xl font-bold text-light-primary dark:text-dark-primary'>
-          {course.courseTitle}
-        </h3>
+    <li className='flex flex-col gap-4 p-4 bg-light-card-bg dark:bg-dark-card-bg rounded-base'>
+      <div className='flex flex-col gap-2'>
+        <div>
+          <h3 className='font-bold text-light-secondary dark:text-dark-secondary'>
+            {course.institution}
+          </h3>
+          <h3 className='text-xl font-bold text-light-primary dark:text-dark-primary'>
+            {course.courseTitle}
+          </h3>
+        </div>
+        <p className='text-light-secondary dark:text-dark-secondary'>
+          {course.period}
+        </p>
+        {course.description && <p>{course.description}</p>}
       </div>
-      <p className='text-light-secondary dark:text-dark-secondary'>
-        {course.period}
-      </p>
-      <p>{course.description}</p>
+      {course.certificate && (
+        <Button
+          variant={course.certificate?.variant}
+          url={course.certificate?.url}
+          className='self-end'
+        >
+          {course.certificate?.cta}
+        </Button>
+      )}
     </li>
   );
 };
