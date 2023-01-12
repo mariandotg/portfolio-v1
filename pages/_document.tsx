4a3d5a92999657c07a27a35cc8b5549e019fde46
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-01-12 07:39:47 -0300

    Fixed google tag manager tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 function Document() {
   const codeToRunOnClient = `
@@ -14,11 +14,11 @@ function Document() {
         .querySelector('html')
         .classList.replace(theme === 'dark' ? 'light' : 'dark', theme);
   })();`;
+
   return (
     <Html className='dark'>
       <Head>
-        <Script
-          strategy='afterInteractive'
+        <script
           id='gtagmanager'
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -27,7 +27,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KHP8X9Q');`,
           }}
-        ></Script>
+        />
         <script
           id='theme-init'
           dangerouslySetInnerHTML={{ __html: codeToRunOnClient }}
