c87e36a786d2cce452e9d73c97204e5cf6e4c26d
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-01-16 11:22:34 -0300

    Refactored Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,10 +7,10 @@ import useIsMounted from '../../hooks/useIsMounted';
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import LanguageIcon from '@mui/icons-material/Language';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
+import BrandLogo from '../../public/mariandotg_logo.svg';
 import Button from '../Buttons/Button';
 
 import { selectData } from '../../app/store/slices/data';
@@ -20,62 +20,75 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const data = useSelector(selectData);
-  const { languagesList } = data.sections.header;
+  const { menu } = data.sections.header;
   const { scrollPercent } = useScroll();
   const { theme, toggleTheme } = useTheme();
   const isMounted = useIsMounted();
 
-  const handleLangChange = () => {
-    setIsOpen((prevValue: boolean) => !prevValue);
-  };
+  const menuItemsList = menu.slice(0, 2);
+  const completeLangsList = menu.slice(2);
+
+  const langList = completeLangsList.filter(
+    (l: ILanguage) => l.slug !== data.language
+  );
+
+  const handleLangChange = () => setIsOpen((prevValue: boolean) => !prevValue);
 
   const changeLanguage = (lang: string) => {
-    if (lang === data.language) return;
-    handleLangChange();
     router.push(`/${lang}`, `/${lang}`, { locale: lang });
+    handleLangChange();
   };
 
-  const currentLanguage = languagesList
-    .filter((l: ILanguage) => l.slug === data.language)[0]
-    .displayName.toLocaleUpperCase();
+  const currentLanguage = completeLangsList.find(
+    (l: ILanguage) => l.slug === data.language
+  );
 
   return (
     <>
       <header className='sticky top-0 z-50'>
-        <div className='flex w-full p-4 bg-light-bg dark:bg-dark-bg'>
-          <div className='relative w-fit'>
-            <button
-              className='flex items-center gap-2 py-4'
-              onClick={handleLangChange}
-            >
-              <LanguageIcon />
-              <span>{currentLanguage}</span>
-              {!isOpen ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
-            </button>
-            {isOpen && (
-              <ul className='absolute w-full px-4 py-2 text-right bg-light-card-bg dark:bg-dark-card-bg rounded-base'>
-                {languagesList.map((l: ILanguage) => {
-                  return (
-                    <li
-                      key={l.id}
-                      value={l.slug}
-                      tabIndex={0}
-                      className={`py-2 ${
-                        l.slug === data.language
-                          ? 'before:content-["●"] before:mr-2 text-light-primary dark:text-dark-primary'
-                          : ''
-                      }`}
-                      onKeyDown={(e) => {
-                        if (e.key === 'Enter') return changeLanguage(l.slug);
-                      }}
-                      onClick={() => changeLanguage(l.slug)}
-                    >
-                      {l.displayName.toLocaleUpperCase()}
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
+        <nav className='flex w-full p-4 bg-light-bg dark:bg-dark-bg'>
+          <BrandLogo
+            className='w-[92px] h-[58px] dark:fill-dark-text py-2 pr-4'
+            viewBox='0 0 92 58'
+          />
+          <div className='flex gap-4 font-bold'>
+            {menuItemsList.map((l: ILanguage) => (
+              <span
+                key={l.id}
+                className='flex items-center gap-2 py-4 cursor-pointer hover:text-light-primary hover:dark:text-dark-primary'
+              >
+                {l.displayName}
+              </span>
+            ))}
+            <div className='relative w-fill'>
+              <div
+                className='flex items-center h-full gap-2 py-4 cursor-pointer hover:text-light-primary hover:dark:text-dark-primary'
+                onClick={handleLangChange}
+              >
+                {currentLanguage.displayName}
+                {!isOpen ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+              </div>
+              {isOpen && (
+                <ul className='absolute px-4 py-2 text-right rounded-lg shadow-lg w-fit bg-light-card-bg dark:bg-dark-card-bg'>
+                  {langList.map((l: ILanguage) => {
+                    return (
+                      <li
+                        key={l.id}
+                        value={l.slug}
+                        tabIndex={0}
+                        className='flex items-center py-2 cursor-pointer w-fill hover:text-light-primary hover:dark:text-dark-primary'
+                        onKeyDown={(e) => {
+                          if (e.key === 'Enter') return changeLanguage(l.slug);
+                        }}
+                        onClick={() => changeLanguage(l.slug)}
+                      >
+                        {l.displayName}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
           </div>
           {isMounted && (
             <Button
@@ -88,7 +101,7 @@ const Header = () => {
               {theme === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
             </Button>
           )}
-        </div>
+        </nav>
         <div className='w-full h-1 bg-light-card-bg dark:bg-dark-card-bg'>
           <div
             className='h-1 transition bg-light-primary dark:bg-dark-primary'
