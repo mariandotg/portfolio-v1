72886b36de6b322d5de13143ca5f5f4842ced6aa
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-24 16:21:52 -0300

    Added cache file

---
+++ cache.ts
@@ -0,0 +1,465 @@
+export const data = {
+  sections: {
+    header: {
+      title: 'Header',
+      key: 'header',
+      language: 'es',
+      id: '3DwTYAzuotuaKTaB1EbYOf',
+      menu: [
+        {
+          displayName: 'Portfolio',
+          id: '4TAQ0FfZn11lFiXIFPurMw',
+        },
+        {
+          displayName: 'Blog',
+          slug: 'blog',
+          id: '7BrelOmLQzSHe75KCdSPaG',
+        },
+        {
+          displayName: 'Español',
+          slug: 'es',
+          id: '52FlIFM8kAzNfixuKWiWHl',
+        },
+        {
+          displayName: 'Inglés',
+          slug: 'en',
+          id: '6J4m7OIVvUrixnOpXnQuc6',
+        },
+      ],
+    },
+    about: {
+      title:
+        'Soy Mariano Guillaume. <br />Desarrollador Fullstack especializado en Next.js. <br />Yo hago Aplicaciones web centrada en el usuario.',
+      key: 'about',
+      language: 'es',
+      description:
+        'Soy un desarrollador full-stack autodidacta con pasión por crear aplicaciones web dinámicas y visualmente atractivas. Me especializo en Next.js y en el stack MERN, con habilidades en desarrollo front-end y back-end. Tengo una pasión por el diseño y llevo un enfoque centrado en el usuario en mis proyectos.',
+      cta: {
+        cta: 'Contactame',
+        url: 'mailto:marianguillaume.m@gmail.com',
+        variant: 'primary',
+      },
+      id: '4rzZ6h9HOPSnpcJ6vtGvzn',
+    },
+    featuredProjects: {
+      title: 'Proyectos destacados:',
+      key: 'featuredProjects',
+      cards: [
+        {
+          image:
+            '//images.ctfassets.net/9lqsfxa4mteg/Q7YvFUTFcxdMhQhv9V1dA/8ecedf8903648e322fd6d26f78cf4335/suono-1.png',
+          name: 'Suono',
+          year: '2022',
+          description:
+            'App web creada con Vite y JS vanilla. Su principal función es cortar audio dentro del navegador, sin ninguna llamada al servidor, esto lo logra utilizando la Web Audio API.',
+          repository: {
+            cta: 'Ver repo',
+            url: 'https://github.com/mariandotg/suono',
+            variant: 'secondary',
+          },
+          site: {
+            cta: 'Ver sitio',
+            url: 'https://suono.vercel.app',
+            variant: 'primary',
+          },
+          id: 'NRrIyxkiYnbV8SV5P8YR0',
+        },
+      ],
+      language: 'es',
+      id: 'eoI7CRV8fwh8aOPELv3BO',
+    },
+    education: {
+      title: 'Educación:',
+      key: 'education',
+      cards: [
+        {
+          institution: 'Alkemy',
+          courseTitle: 'Aceleración React Developer',
+          period: 'Abril 2022 - Julio 2022',
+          description:
+            'Haciendo este curso pude obtener una idea más amplia sobre como trabajar en un equipo de desarrollo, y a como manejarme correctamente mientras trabajo con metodologias ágiles, además de la gran importancia de las soft skills.',
+          certificate: {
+            cta: 'Ver certificado',
+            url: 'https://assets.alkemy.org/certificates/google-oauth2%7C115297975985479115679/ae5142d1-b0e0-4bcd-9680-9ade6a4ad69d.pdf',
+            variant: 'primary',
+          },
+          id: '2mr85x1rwc4cX6tohxEMpa',
+        },
+        {
+          institution: 'The University of Helsinki',
+          courseTitle: 'Full Stack Open',
+          period: 'Actualmente estudiando',
+          description:
+            'Haciendo este curso pude asentar conocimientos sobre React.js, Node.js, Express.js, y Mongodb (con Mongoose) como tambien del funcionamiento de las web apps, su comunicación con el server y las bases de datos, además de estudiar distintos tipos de arquitectura y aprender a testear apps de Node.js y de React.js con Jest.',
+          certificate: {
+            cta: 'Ver certificado',
+            url: 'https://studies.cs.helsinki.fi/stats/api/certificate/fullstackopen/en/4de247074459d8eea5fd1fbb05089e56',
+            variant: 'primary',
+          },
+          id: '4KqTEqSzKsWuwmoSjufn1q',
+        },
+      ],
+      language: 'es',
+      id: '2mG9UOz3HmSgkn7CVPYZB9',
+    },
+    contact: {
+      title: 'Contacto:',
+      key: 'contact',
+      cards: [
+        {
+          label: 'Asunto',
+          placeholder: '¡Hola mundo!',
+          name: 'subject',
+          type: 'text',
+          id: 'snD6hFwlndwDC9kKIuqAA',
+        },
+        {
+          label: 'Email',
+          placeholder: 'ejemplo@compañia.com',
+          name: 'from',
+          type: 'text',
+          id: '68djkAT0gLGQHXpIHmMoMx',
+        },
+        {
+          label: 'Mensaje',
+          placeholder: 'Escribe tu mensaje aquí.',
+          name: 'message',
+          type: 'textarea',
+          id: '1rai3QlGTcyADu1svaeHX5',
+        },
+      ],
+      language: 'es',
+      cta: {
+        cta: 'Enviar',
+        url: 'https://www.alkemy.com',
+        variant: 'primary',
+      },
+      feedback: {
+        required: 'Campo obligatorio.',
+        error: {
+          minCant: 'El número mínimo de caracteres es 4.',
+          validEmail: 'Debes escribir un email válido.',
+        },
+      },
+      id: '4BZxOX8M2bjzrWwiiIUvTX',
+    },
+    info: {
+      title: 'Info:',
+      key: 'info',
+      cards: [
+        {
+          label: 'Fecha de nacimiento:',
+          item: '6 de agosto, 2003',
+          id: '2Lps0xv9nbAWYvXgNTumbm',
+        },
+        {
+          label: 'Nacionalidad:',
+          item: 'Argentino',
+          id: '3AZ851KEhwVP9WFYyIwDyq',
+        },
+        {
+          label: 'Email:',
+          item: 'marianguillaume.m@gmail.com',
+          id: '6uHV4P6Sf7AJAKHWzpEIMu',
+        },
+      ],
+      language: 'es',
+      id: '4Tv3QFqd6USv5H1xOXw5eX',
+    },
+    footer: {
+      title: '¡Sentite libre de contactarme!',
+      key: 'footer',
+      language: 'es',
+      id: '2yeR46N1JEmPltdrNmlOPp',
+      socialMedia: [
+        {
+          url: 'https://www.linkedin.com/in/marianoguillaume/',
+          icon: 'LinkedInIcon',
+          id: '34xz5ZUuptgmiPHwuGSvML',
+        },
+        {
+          url: 'https://github.com/mariandotg',
+          icon: 'GitHubIcon',
+          id: '39zT8BImy1dI6WwqodqlCa',
+        },
+      ],
+    },
+    jobExperience: {
+      title: 'Experiencia laboral:',
+      key: 'jobExperience',
+      cards: [
+        {
+          company: 'Freelance',
+          position: 'Diseñador gráfico',
+          period: 'Abril 2019 - Abril 2022',
+          description:
+            'Esta experiencia es valiosa en mi trabajo como desarrollador, ya que me permite enfocar en el usuario y en el diseño visual de los proyectos. Mi experiencia previa en diseño gráfico me ayuda a crear interfaces de usuario atractivas y fáciles de navegar, utilizando teoría del color y tipografía para crear diseños coherentes y consistentes. Mi habilidad para pensar creativamente también me permite encontrar soluciones innovadoras a los problemas de diseño. ',
+          id: 'AFSyhXVXwmvNgavTrgnDf',
+        },
+      ],
+      language: 'es',
+      id: '6RnwhnXtvRbLLE1lxSFL8a',
+    },
+    skills: {
+      title: 'Habilidades:',
+      key: 'skills',
+      cards: [
+        {
+          subtitle: 'Frontend',
+          techs:
+            'HTML, CSS3, Javascript, React, Next.js, Typescript, Tailwindcss, Bootstrap, Styled-components, Redux',
+          id: '5nuvtv4ZgQj9oWD5BtTJI4',
+        },
+        {
+          subtitle: 'Backend',
+          techs:
+            'Node.js, Express.js, Mongodb, Java, Firebase, Strapi, Contentful',
+          id: '2vzvfVFkuvmDxe8OKCQSTJ',
+        },
+        {
+          subtitle: 'Otras herramientas',
+          techs:
+            'Figma, Adobe Photoshop, Adobe Illustrator, Git, Postman, NPM, Yarn',
+          id: '5oMznAtEsmn0MCv3wwERHO',
+        },
+      ],
+      language: 'es',
+      id: '7rKHtqLPhQcJYBxfr8NIYg',
+    },
+  },
+  loading: 'false',
+  language: 'es',
+};
+
+export const dataEN = {
+  sections: {
+    header: {
+      title: 'Header',
+      key: 'header',
+      language: 'en',
+      id: '4h0AlaQYlRKTBkDQy3GKkB',
+      menu: [
+        {
+          displayName: 'Portfolio',
+          id: '4TAQ0FfZn11lFiXIFPurMw',
+        },
+        {
+          displayName: 'Blog',
+          slug: 'blog',
+          id: '7BrelOmLQzSHe75KCdSPaG',
+        },
+        {
+          displayName: 'English',
+          slug: 'en',
+          id: '4vllkKhN2ySEHuSdv7gNRN',
+        },
+        {
+          displayName: 'Spanish',
+          slug: 'es',
+          id: '1595tyW0gILSMlQSgU15Sd',
+        },
+      ],
+    },
+    about: {
+      title:
+        'I am Mariano Guillaume. <br />Fullstack Developer Specializing in Next.js. <br />I make User-centered Web Applications.',
+      key: 'about',
+      language: 'en',
+      description:
+        'I am a self-taught full-stack developer with a passion for creating dynamic and visually pleasing web applications. I specialize in Next.js and MERN stack, with skills in front-end and back-end development. I have a passion for design and bring a user-centered approach to my projects.',
+      cta: {
+        cta: 'Contact me',
+        url: 'mailto:marianguillaume.m@gmail.com',
+        variant: 'primary',
+      },
+      id: '7dgXfQFB05sGL0QhKkeiJ3',
+    },
+    featuredProjects: {
+      title: 'Featured Projects:',
+      key: 'featuredProjects',
+      cards: [
+        {
+          image:
+            '//images.ctfassets.net/9lqsfxa4mteg/Q7YvFUTFcxdMhQhv9V1dA/8ecedf8903648e322fd6d26f78cf4335/suono-1.png',
+          name: 'Suono',
+          year: '2022',
+          description:
+            'Web app created with Vite and vanilla JS. Its main function is to cut audio within the browser, without any calls to the server, this is achieved using the Web Audio API.',
+          repository: {
+            cta: 'View repo',
+            url: 'https://github.com/mariandotg/suono',
+            variant: 'secondary',
+          },
+          site: {
+            cta: 'View site',
+            url: 'https://suono.vercel.app',
+            variant: 'primary',
+          },
+          id: '6SjmIkOAztn1Y3KAzQSwFt',
+        },
+      ],
+      language: 'en',
+      id: '5ypgJ2fDCRNcMHqqOZlKNO',
+    },
+    education: {
+      title: 'Education:',
+      key: 'education',
+      cards: [
+        {
+          institution: 'Alkemy',
+          courseTitle: 'React Developer Acceleration',
+          period: 'April 2022 - July 2022',
+          description:
+            'Doing this course I was able to get a better experience working in a development team, and how to work with agile methodologies, in addition to the great importance of soft skills.',
+          certificate: {
+            cta: 'View certificate',
+            url: 'https://assets.alkemy.org/certificates/google-oauth2%7C115297975985479115679/ae5142d1-b0e0-4bcd-9680-9ade6a4ad69d.pdf',
+            variant: 'primary',
+          },
+          id: 'n1tRMpLGs0RaRaL8jmILY',
+        },
+        {
+          institution: 'The University of Helsinki',
+          courseTitle: 'Full Stack Open',
+          period: 'Currently studying',
+          description:
+            'I was able to establish my prior knowledge about React.js, Node.js, Express.js, and Mongodb (with Mongoose) as well as the internal functioning of the web apps, their communication with the server and the databases, in addition to studying different types of architecture and learning to test Node.js and React.js apps with Jest.',
+          certificate: {
+            cta: 'View certificate',
+            url: 'https://studies.cs.helsinki.fi/stats/api/certificate/fullstackopen/en/4de247074459d8eea5fd1fbb05089e56',
+            variant: 'primary',
+          },
+          id: '3AvVMQFSUepVkAOeyslehw',
+        },
+      ],
+      language: 'en',
+      id: 'HJI2IZlF99eHyl7qC0LHG',
+    },
+    contact: {
+      title: 'Contact:',
+      key: 'contact',
+      cards: [
+        {
+          label: 'Subject',
+          placeholder: 'Hello world!',
+          name: 'subject',
+          type: 'text',
+          id: '3v1hGxzOpzSuLowsrVa5RE',
+        },
+        {
+          label: 'Email',
+          placeholder: 'example@company.com',
+          name: 'from',
+          type: 'text',
+          id: '1jTSN9sm8IydrHqzUbRBWT',
+        },
+        {
+          label: 'Message',
+          placeholder: 'Write a message here!',
+          name: 'message',
+          type: 'textarea',
+          id: '3CqKxdVoq366eyG3QklN8E',
+        },
+      ],
+      language: 'en',
+      cta: {
+        cta: 'Submit',
+        url: 'https://www.alkemy.com',
+        variant: 'primary',
+      },
+      feedback: {
+        required: 'Obligatory field.',
+        error: {
+          minCant: 'The minimum number of characters is 4.',
+          validEmail: 'You must write a valid email.',
+        },
+      },
+      id: '6hLnTGTH0BTJXKOxaWtBe8',
+    },
+    jobExperience: {
+      title: 'Job Experience:',
+      key: 'jobExperience',
+      cards: [
+        {
+          company: 'Freelance',
+          position: 'Graphic designer',
+          period: 'April 2019 - April 2022',
+          description:
+            'This experience is valuable in my work as a developer, as it allows me to focus on the user and the visual design of projects. My previous experience in graphic design helps me create attractive and easy-to-navigate user interfaces, using color theory and typography to create cohesive and consistent designs. My ability to think creatively also allows me to find innovative solutions to design problems.',
+          id: '107sOggdRWgNbvK3oOfdyl',
+        },
+      ],
+      language: 'en',
+      id: '68yMbKUFSUPloWeqWQ56fT',
+    },
+    skills: {
+      title: 'Skills:',
+      key: 'skills',
+      cards: [
+        {
+          subtitle: 'Frontend',
+          techs:
+            'HTML, CSS3, Javascript, React, Next.js, Typescript, Tailwindcss, Bootstrap, Styled-components, Redux',
+          id: '5nuvtv4ZgQj9oWD5BtTJI4',
+        },
+        {
+          subtitle: 'Backend',
+          techs:
+            'Node.js, Express.js, Mongodb, Java, Firebase, Strapi, Contentful',
+          id: '2vzvfVFkuvmDxe8OKCQSTJ',
+        },
+        {
+          subtitle: 'Other tools',
+          techs:
+            'Figma, Adobe Photoshop, Adobe Illustrator, Git, Postman, NPM, Yarn',
+          id: '29DV6dkAdXDYH8KvwXyPuM',
+        },
+      ],
+      language: 'en',
+      id: '2ttBYaeokrZoFBNKszD0hO',
+    },
+    footer: {
+      title: 'Feel free to contact me!',
+      key: 'footer',
+      language: 'en',
+      id: '3YJLmOCOi1zTPlYVfrFGcN',
+      socialMedia: [
+        {
+          url: 'https://www.linkedin.com/in/marianoguillaume/',
+          icon: 'LinkedInIcon',
+          id: '34xz5ZUuptgmiPHwuGSvML',
+        },
+        {
+          url: 'https://github.com/mariandotg',
+          icon: 'GitHubIcon',
+          id: '39zT8BImy1dI6WwqodqlCa',
+        },
+      ],
+    },
+    info: {
+      title: 'Info:',
+      key: 'info',
+      cards: [
+        {
+          label: 'Date of birth:',
+          item: 'August 6th, 2003',
+          id: '4LvExcHlgiasOVhxCD4BuN',
+        },
+        {
+          label: 'Nationality:',
+          item: 'Argentinian',
+          id: '6WDh5a7c6th5bdsgMdxZWO',
+        },
+        {
+          label: 'Email:',
+          item: 'marianguillaume.m@gmail.com',
+          id: '6uHV4P6Sf7AJAKHWzpEIMu',
+        },
+      ],
+      language: 'en',
+      id: '5kK2zGuXoRNQUkmTyWlxse',
+    },
+  },
+  loading: 'false',
+  language: 'en',
+};
